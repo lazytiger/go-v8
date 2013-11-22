88e06554c89b28897be4fbe1cefdd9297a1dc11c
realint <realint@qq.com>
2013-11-22 10:49:54 +0800

    add get.sh

---
+++ get.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo >&2 "useage: ./get.sh the_install_dir"
+	exit
+fi
+
+install_dir=$1
+
+mkdir $install_dir
+
+cd $install_dir
+export GOPATH=`pwd`
+
+mkdir -p bin pkg src/github.com/realint/go-v8
+cd src/github.com/realint/go-v8
+
+git init
+git remote add origin https://github.com/realint/go-v8
+git pull origin master
+
+./install.sh
